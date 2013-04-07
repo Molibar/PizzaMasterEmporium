71a05e2815b4d5d55b87783162874dec2b9705ed
Christer Brännström <christer.brannstrom@gmail.com>
2013-04-07 12:33:46 +0100

    Forcing rebuild

---
+++ PizzaMasterEmporium.Domain/OrderContext/Models/Order.cs
@@ -0,0 +1,25 @@
+﻿namespace PizzaMasterEmporium.Domain.OrderContext.Models
+{
+    public class Order
+    {
+        public PizzaSpec[] PizzaSpecs { get; set; }
+    }
+
+    public class PizzaSpec
+    {
+        public string Id { get; set; }
+        public Size Size { get; set; }
+        public Ingredient[] Ingredients { get; set; }
+    }
+
+    public class Size
+    {
+        public int Inches { get; set; }
+    }
+
+    public class Ingredient
+    {
+        public int Type { get; set; }
+        public bool Extra { get; set; }
+    }
+}
